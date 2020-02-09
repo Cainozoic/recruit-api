2a988b4f830df25fcc45000bfa6201a7d15b3219
macbookpro <9941416@qq.com>
2020-02-09 16:38:44 +0800

    init

---
+++ src/main/java/top/wy/RecruitApiApplication.java
@@ -0,0 +1,13 @@
+package top.wy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/top/wy/RecruitApiApplicationTests.java
@@ -0,0 +1,13 @@
+package top.wy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
